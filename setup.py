84db407afbf4911c1595815dc1b970e4090f1293
Najib <najibadam145@gmail.com>
2025-11-09 16:20:15 +0100

    fix the version number and package name

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="Mixstable",
-    version="0.1.3",
+    version="0.1.1",
     description="Parameter estimation tools for one-dimensional stable distributions and their mixtures.",
     long_description=long_description,
     long_description_content_type="text/markdown",
